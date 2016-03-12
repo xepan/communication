31d4f5224271252b425261c4a28df644cda90881
vijay mali <guddavijay@gmail.com>
2016-03-12 18:44:00 +0530

    Email communication model add field callback & email block layout seprate jade & view Files

--- page/emails.php
+++ page/emails.php
@@ -8,7 +8,7 @@ class page_emails extends \Page{
 	function init(){
 		parent::init();
 		/*Emails Header*/
-		// $this->add('xepan\communication\View_EmailHeader',null,'email_header');
+		$this->add('xepan\communication\View_EmailHeader',null,'email_header');
 		
 		/*Emails Navigation*/
 		
@@ -18,6 +18,15 @@ class page_emails extends \Page{
 		$label_view->setModel('xepan\base\Epan_EmailSetting');
 		/*Email List*/
 		$email_model=$this->add('xepan\communication\Model_Communication_Email');
+
+		$email_model->add('misc/Field_Callback','callback_date')->set(function($m){
+			if(date('Y-m-d',strtotime($m['created_at']))==date('Y-m-d',strtotime($this->app->now))){
+				return $m['created_at']=date('h:i:a',strtotime($m['created_at']));	
+			}
+			return $m['created_at']=date('M-d',strtotime($m['created_at']));
+		});
+
+
 		
 		$email_view=$this->add('xepan\communication\View_Lister_EmailsList',null,'email_lister');
 		$email_view->setModel($email_model);
