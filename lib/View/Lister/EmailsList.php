a02df8394232d1c27def7734b898c4328a25255e
vijay mali <guddavijay@gmail.com>
2016-03-18 16:48:59 +0530

    add formatRow to add or remove class

--- lib/View/Lister/EmailsList.php
+++ lib/View/Lister/EmailsList.php
@@ -2,7 +2,14 @@
 namespace xepan\communication;
 
 class View_Lister_EmailsList extends \CompleteLister{
-		
+	function formatRow(){
+		if($this->model['is_starred']){
+			$this->current_row['starred']='starred';
+		}else{
+			$this->current_row['starred']='';
+		}
+		parent::formatRow();
+	}
 	function defaultTemplate(){
 		return ['view/emails/list'];
 	}
