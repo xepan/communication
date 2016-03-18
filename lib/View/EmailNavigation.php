6618515d546aced744dcc8c82e0641e763ca15bc
vijay mali <guddavijay@gmail.com>
2016-03-18 16:48:39 +0530

    Email type to add counts

--- lib/View/EmailNavigation.php
+++ lib/View/EmailNavigation.php
@@ -2,7 +2,35 @@
 namespace xepan\communication;
 
 class View_EmailNavigation extends \View{
-	
+	function init(){
+		parent::init();
+		$contact_inbox=$this->add('xepan\communication\Model_Communication_Email_ContactReceivedEmail');
+		$contact_count=$contact_inbox->count()->getOne();
+		// throw new \Exception($contact_count, 1);
+		$this->template->trySet('contat_inbox_count',$contact_count);
+		
+		$inbox=$this->add('xepan\communication\Model_Communication_Email_Received');
+		$inbox_count=$inbox->count()->getOne();
+		$this->template->trySet('inbox_count',$inbox_count);
+
+		$sent=$this->add('xepan\communication\Model_Communication_Email_Sent');
+		$sent_count=$sent->count()->getOne();
+		$this->template->trySet('sent_email_count',$sent_count);
+		
+		$draft=$this->add('xepan\communication\Model_Communication_Email_Draft');
+		$draft_count=$draft->count()->getOne();
+		$this->template->trySet('draft_count',$draft_count);
+		
+		$starred=$this->add('xepan\communication\Model_Communication_Email_Starred');
+		$starred_count=$starred->count()->getOne();
+		$this->template->trySet('starred_count',$starred_count);
+		
+		$trashed=$this->add('xepan\communication\Model_Communication_Email_Trashed');
+		$trashed_count=$trashed->count()->getOne();
+		$this->template->trySet('trash_count',$trashed_count);
+
+
+	}
 	function getJSID(){
 		return 'email-nav-items';
 	}
