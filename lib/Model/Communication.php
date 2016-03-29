1f5b0e4cde784e4c4c3b8a258f396872b150c215
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-03-29 13:27:34 +0530

    added related_id field

--- lib/Model/Communication.php
+++ lib/Model/Communication.php
@@ -37,6 +37,8 @@ class Model_Communication extends \xepan\base\Model_Document{
 		$comm_j->addField('tags');
 		$comm_j->addField('direction');
 		$comm_j->addField('communication_type');
+
+		$comm_j->addField('related_id'); // Can be used anywhere as per requirement
 		
 		$comm_j->addField('mailbox');
 		$comm_j->addField('is_starred')->type('boolean')->defaultValue(false);
