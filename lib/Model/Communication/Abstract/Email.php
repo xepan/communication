3145a56b0c7b7f9c43097f98eae534786233a3f5
vijay mali <guddavijay@gmail.com>
2016-04-04 17:17:20 +0530

    after save hook checked if obj is array so json encode convert

--- lib/Model/Communication/Abstract/Email.php
+++ lib/Model/Communication/Abstract/Email.php
@@ -43,9 +43,10 @@ class Model_Communication_Abstract_Email extends Model_Communication{
 		});
 
 		$this->addHook('afterLoad',function($m){
-
 			$m['from_raw'] = json_decode($m['from_raw'],true);
-			$m['to_raw'] = json_decode($m['to_raw'],true);
+			$this['to_raw'] = json_decode($m['to_raw'],true);
+			// var_dump($this['to_raw']);
+			// exit;
 			$m['cc_raw'] = json_decode($m['cc_raw'],true);
 			$m['bcc_raw'] = json_decode($m['bcc_raw'],true);
 			$m['title'] = $m['subject'] = $m['title']?:'(no subject)';
@@ -55,10 +56,14 @@ class Model_Communication_Abstract_Email extends Model_Communication{
 		});
 		
 		$this->addHook('beforeSave',function($m){
-			$m['to_raw'] = json_encode($m['to_raw']);
-			$m['from_raw'] = json_encode($m['from_raw']);
-			$m['cc_raw'] = json_encode($m['cc_raw']);
-			$m['bcc_raw'] = json_encode($m['bcc_raw']);
+			if(is_array($m['to_raw']))
+				$m['to_raw'] = json_encode($m['to_raw']);
+			if(is_array($m['from_raw']))
+				$m['from_raw'] = json_encode($m['from_raw']);
+			if(is_array($m['cc_raw']))
+				$m['cc_raw'] = json_encode($m['cc_raw']);
+			if(is_array($m['bcc_raw']))
+				$m['bcc_raw'] = json_encode($m['bcc_raw']);
 			$m['title'] = $m['title']?:("(no subject)");
 		});
 
