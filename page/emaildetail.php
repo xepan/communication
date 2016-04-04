58734f4e979bb07bb8d2745bebd033cb6fe9fc34
vijay mali <guddavijay@gmail.com>
2016-04-04 17:17:53 +0530

    email model load to get email id

--- page/emaildetail.php
+++ page/emaildetail.php
@@ -6,10 +6,12 @@ class page_emaildetail extends \Page{
 	function init(){
 		parent::init();
 		$email_id=$this->api->stickyGET('email_id');
+		// throw new \Exception($email_id, 1);
+		
 		
 		$email_model=$this->add('xepan\communication\Model_Communication_Email_Received');
-		$email_model->addCondition('id',$_GET['email_id']);
-		$email_model->tryLoadAny();
+		$email_model->load($_GET['email_id']);
+		// $email_model->tryLoadAny();
 		$email_detail=$this->add('xepan\communication\View_EmailDetail');
 
 		$email_detail->setModel($email_model);
