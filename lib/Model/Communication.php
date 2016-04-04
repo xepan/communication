f9dec68192c43bea101e7697e09022999ad77b99
vijay mali <guddavijay@gmail.com>
2016-04-04 17:16:44 +0530

    communication model extends model_table

--- lib/Model/Communication.php
+++ lib/Model/Communication.php
@@ -11,40 +11,40 @@
 
 namespace xepan\communication;
 
-class Model_Communication extends \xepan\base\Model_Document{
-
+class Model_Communication extends \xepan\base\Model_Table{
+	public $table="communication"
 	function init(){
 		parent::init();
 		
-		$comm_j = $this->join('communication.document_id');
-		$comm_j->hasOne('xepan\base\Contact','from_id');
-		$comm_j->hasOne('xepan\base\Contact','to_id');
-		$comm_j->hasOne('xepan\base\Document','related_document_id');
+		// $this = $this->join('communication.document_id');
+		$this->hasOne('xepan\base\Contact','from_id');
+		$this->hasOne('xepan\base\Contact','to_id');
+		$this->hasOne('xepan\base\Document','related_document_id');
 		
-		$comm_j->addField('uid');
-		$comm_j->addField('uuid');
-		$comm_j->addField('reply_to');
+		$this->addField('uid');
+		$this->addField('uuid');
+		$this->addField('reply_to');
 		
-		$comm_j->addField('from_raw')->defaultValue([]);
-		$comm_j->addField('to_raw')->defaultValue([]);
-		$comm_j->addField('flags');
-		$comm_j->addField('cc_raw')->defaultValue([]);
-		$comm_j->addField('bcc_raw')->defaultValue([]);
+		$this->addField('from_raw')->defaultValue([]);
+		$this->addField('to_raw')->defaultValue([]);
+		$this->addField('flags');
+		$this->addField('cc_raw')->defaultValue([]);
+		$this->addField('bcc_raw')->defaultValue([]);
 
-		$comm_j->addField('title');
-		$comm_j->addField('description');
+		$this->addField('title');
+		$this->addField('description');
 
-		$comm_j->addField('tags');
-		$comm_j->addField('direction');
-		$comm_j->addField('communication_type');
+		$this->addField('tags');
+		$this->addField('direction');
+		$this->addField('communication_type');
 
-		$comm_j->addField('related_id'); // Can be used anywhere as per requirement
-		$comm_j->addField('sent_on')->type('date'); // Can be used anywhere as per requirement
+		$this->addField('related_id'); // Can be used anywhere as per requirement
+		$this->addField('sent_on')->type('date'); // Can be used anywhere as per requirement
 		
-		$comm_j->addField('mailbox');
-		$comm_j->addField('is_starred')->type('boolean')->defaultValue(false);
+		$this->addField('mailbox');
+		$this->addField('is_starred')->type('boolean')->defaultValue(false);
 
-		$comm_j->addField('detailed')->type('boolean')->defaultValue(false);
+		$this->addField('detailed')->type('boolean')->defaultValue(false);
 
 		$this->addCondition('type','Communication');
 	}
