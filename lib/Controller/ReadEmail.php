4dafcfa8a002be9b519adcde870d522eb22fe44b
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-06 17:06:20 +0530

    Read Email working and throwing email_fetched hook now app wide

--- lib/Controller/ReadEmail.php
+++ lib/Controller/ReadEmail.php
@@ -1,12 +1,36 @@
 <?php
 
+/*
 
+ALL - return all messages matching the rest of the criteria
+ANSWERED - match messages with the \\ANSWERED flag set
+BCC "string" - match messages with "string" in the Bcc: field
+BEFORE "date" - match messages with Date: before "date"
+BODY "string" - match messages with "string" in the body of the message
+CC "string" - match messages with "string" in the Cc: field
+DELETED - match deleted messages
+FLAGGED - match messages with the \\FLAGGED (sometimes referred to as Important or Urgent) flag set
+FROM "string" - match messages with "string" in the From: field
+KEYWORD "string" - match messages with "string" as a keyword
+NEW - match new messages
+OLD - match old messages
+ON "date" - match messages with Date: matching "date"
+RECENT - match messages with the \\RECENT flag set
+SEEN - match messages that have been read (the \\SEEN flag is set)
+SINCE "date" - match messages with Date: after "date"
+SUBJECT "string" - match messages with "string" in the Subject:
+TEXT "string" - match messages with text "string"
+TO "string" - match messages with "string" in the To:
+UNANSWERED - match messages that have not been answered
+UNDELETED - match messages that are not deleted
+UNFLAGGED - match messages that are not flagged
+UNKEYWORD "string" - match messages that do not have the keyword "string"
+UNSEEN - match messages which have not been read yet
+
+ */
 
 namespace xepan\communication;
 
-// use Ddeboer\Imap;
-// use Ddeboer\Imap\Server;
-// use Ddeboer\Imap\SearchExpression;
 
 class Controller_ReadEmail extends \AbstractController {
 	
@@ -20,195 +44,92 @@ class Controller_ReadEmail extends \AbstractController {
 		if(!$this->email_setting or !($this->email_setting instanceof \xepan\base\Model_Epan_EmailSetting)){
 			throw $this->exception('Please provide email_setting value as loaded xepan\base\Model_Epan_EmailSetting instance');
 		}
-
-
-		// $server = new Server(
-		//     $hostname, // required
-		//     $port,     // defaults to 993
-		//     $flags,    // defaults to '/imap/ssl/validate-cert'
-		//     $parameters
-		// );
-		// $this->imap = new \Ddeboer\Imap\Server(
-		//     $this->email_setting['imap_email_host'], 
-		//     $this->email_setting['imap_email_port'], 
-		//     $this->email_setting['imap_flags']);
-
-		// $this->connection = $this->imap->authenticate($this->email_setting['imap_email_username'],$this->email_setting['imap_email_password']);
 	}
 
-	/*
-	
-	ALL - return all messages matching the rest of the criteria
-	ANSWERED - match messages with the \\ANSWERED flag set
-	BCC "string" - match messages with "string" in the Bcc: field
-	BEFORE "date" - match messages with Date: before "date"
-	BODY "string" - match messages with "string" in the body of the message
-	CC "string" - match messages with "string" in the Cc: field
-	DELETED - match deleted messages
-	FLAGGED - match messages with the \\FLAGGED (sometimes referred to as Important or Urgent) flag set
-	FROM "string" - match messages with "string" in the From: field
-	KEYWORD "string" - match messages with "string" as a keyword
-	NEW - match new messages
-	OLD - match old messages
-	ON "date" - match messages with Date: matching "date"
-	RECENT - match messages with the \\RECENT flag set
-	SEEN - match messages that have been read (the \\SEEN flag is set)
-	SINCE "date" - match messages with Date: after "date"
-	SUBJECT "string" - match messages with "string" in the Subject:
-	TEXT "string" - match messages with text "string"
-	TO "string" - match messages with "string" in the To:
-	UNANSWERED - match messages that have not been answered
-	UNDELETED - match messages that are not deleted
-	UNFLAGGED - match messages that are not flagged
-	UNKEYWORD "string" - match messages that do not have the keyword "string"
-	UNSEEN - match messages which have not been read yet
 
-	 */
-
-	function fetch($mail_box, $conditions=null){
+	function fetch($mailbox_name, $conditions=null){
 		$imap_email_host = $this->email_setting['imap_email_host']; 
 		$imap_email_port = $this->email_setting['imap_email_port'];
 		$imap_email_username = $this->email_setting['imap_email_username']; 
 		$imap_email_password = $this->email_setting['imap_email_password']; 
 		$imap_flags = $this->email_setting['imap_flags'];
 
-		$mailbox = new ImapMailbox('{'.$imap_email_host.':'.$imap_email_port.$imap_flags.'}'.$mail_box, $imap_email_username, $imap_email_password, "upload", 'utf-8');
+		$mailbox = new ImapMailbox('{'.$imap_email_host.':'.$imap_email_port.$imap_flags.'}'.$mailbox_name, $imap_email_username, $imap_email_password, "upload", 'utf-8');
 		
-		$mails = array();
-		// var_dump($mailbox->getMailboxInfo());
-		// return;
-		// Get some mail
+		$return=[];
+
 		try{
-			$conditions = $conditions?:'UNSEEN'/*.date('d-M-Y',strtotime('-1 day'))*/;
+			$conditions = $conditions?:'UNSEEN';
 			$mailsIds = $mailbox->searchMailBox($conditions);
-			// var_dump($mailsIds);
-			// exit;
+
 			if(!$mailsIds) {
 				$mailbox->disconnect();
-			}else{
-				$mail_m = $this->add('xepan\communication\Model_Communication_Email_Received');
-				$i=1;
-				$fetch_email_array = array();
-				foreach ($mailsIds as $mailId) {
-					$mail = $mailbox->getMail($mailId);
-					// var_dump($mail->getAttachments());
-					$mail_m['uid']= $mail->id;
-					
-					$fetched = $this->add('xepan\communication\Model_Communication_Email_Received');
-					
-	
-					$fetched->addCondition('uid',$mail->id);
-					$fetched->addCondition('from_raw',$mail->fromAddress);
-					$fetched->addCondition('to_raw',$mail->to);
-					$fetched->addCondition('created_at',$mail->date);
-					$fetched->tryLoadAny();
-					if($fetched->loaded()) continue;
-					
-					$from_raw = json_decode($fetched['from_raw']);
-					$from=['name'=>$mail->fromName,'email'=>$mail->fromAddress];
-					$from_raw[] = $from;
-					// var_dump($mail->to);
-					// exit;
-
-					/*Fetch TO Email Array & Convert To array name or email format*/
-					$to_email_arry = $mail->to;
-					$to_raw = [];
-					foreach ($to_email_arry as $key => $value) {
-						$temp = ['name'=>$value,'email'=>$key];
-						$to_raw[] = $temp;
-					}
-
-					/*Fetch CC Email Array & Convert To array name or email format*/
-					$cc_email_array=$mail->cc;
-					$cc_raw=[];
-					foreach ($cc_email_array as $key => $cc_value) {
-						$cc=['name'=>$cc_value,'email'=>$key];
-						$cc_raw[]=$cc;
-					}
-					// var_dump($cc_raw);
-					// exit;
-					
-					$mail_m['mailbox']=$this->email_setting['imap_email_username'].'#'.$mail_box;
-					$mail_m['created_at']= $mail->date;
-					$mail_m['created_at']= $mail->date;
-					$mail_m['from_raw'] = json_encode($from_raw); //$mail->fromAddress;
-					$mail_m['to_raw'] =json_encode($to_raw); 
-					$mail_m['cc_raw'] =json_encode($cc_raw); 
-					$mail_m['title'] = $mail->subject;
-					$mail_m['description'] = $mail->textHtml?:$mail->textPlain;
-					$mail_m['uid'] = $mail->id;
-					$mail_m['direction'] = 'In';
-					$mail_m['flags'] = $conditions;
-					$mail_m->findContact(true);
-					// $mail_m['from_name'] = $mail->fromName;
-					$mail_m->save();
-					$fetch_email_array[] = $mail_m->id;
-					
-					//MAIL ATTACHMENT 
-					$attachments = $mail->getAttachments();
-					foreach ($attachments as $attach) {
-						$file =	$this->add('filestore/Model_File',array('policy_add_new_type'=>true,'import_mode'=>'move','import_source'=>$attach->filePath));
-						$file['filestore_volume_id'] = $file->getAvailableVolumeID();
-						$file['original_filename'] = $attach->name;
-						$file->save();
-						$mail_m->addAttachment($file->id);
-					}
-
-					$mail_m->unload();
-					$i++;
-				}
+				return $return;
 			}
 
+			$i=1;
+			$fetch_email_array = array();
+			foreach ($mailsIds as $mailId) {
+				$fetched_mail = $mailbox->getMail($mailId);
+
+				
+				$mail_m = $this->add('xepan\communication\Model_Communication_Email_Received');
+				$mail_m->addCondition('uid',$fetched_mail->id);
+				$mail_m->addCondition('mailbox',$this->email_setting['imap_email_username'].'#'.$mailbox_name);
+				$mail_m->tryLoadAny();
+				
+				if($mail_m->loaded()) continue;
+				
+				$mail_m->setFrom($fetched_mail->fromAddress,$fetched_mail->fromName);
+
+				/*Fetch TO Email Array & Convert To array name or email format*/
+				$to_email_arry = $fetched_mail->to;
+				foreach ($to_email_arry as $email => $name) {
+					$mail_m->addTo($email,$name);
+				}
+
+				/*Fetch CC Email Array & Convert To array name or email format*/
+				$cc_email_array=$fetched_mail->cc;
+				foreach ($cc_email_array as $email => $name) {
+					$mail_m->addCc($email,$name);
+				}
+				if(isset($fetched_mail->bcc)){
+					$bcc_email_array=$fetched_mail->bcc;
+					foreach ($bcc_email_array as $email => $name) {
+						$mail_m->addBcc($email,$name);
+					}
+				}
+				
+				$mail_m['created_at']= $fetched_mail->date;
+				$mail_m['title'] = $fetched_mail->subject;
+				$mail_m['description'] = $fetched_mail->textHtml?:$fetched_mail->textPlain;
+				$mail_m['flags'] = $conditions;
+				$mail_m->findContact(false);
+				$mail_m->save();
+				$fetch_email_array[] = $mail_m->id;
+				
+				if(!isset($return['fetched_emails_from']))
+					$return['fetched_emails_from'] = $mail_m->id;
+				
+				//MAIL ATTACHME  NT 
+				$attachments = $fetched_mail->getAttachments();
+				foreach ($attachments as $attach) {
+					$file =	$this->add('filestore/Model_File',array('policy_add_new_type'=>true,'import_mode'=>'move','import_source'=>$attach->filePath));
+					$file['filestore_volume_id'] = $file->getAvailableVolumeID();
+					$file['original_filename'] = $attach->name;
+					$file->save();
+					$mail_m->addAttachment($file->id);
+				}
+
+				$mail_m->unload();
+				$i++;
+			}
+			
 		}catch(\Exception $e){
 			$mailbox->disconnect();
 			throw $e;
 		}
+
+		return $return;
 	}
-
-	// function fetch($mail_box,$query='Ddeboer\Imap\Search\Flag\Unseen',$start=null,$range=null,$or_search=false,$body=true){
-	// 	$mailbox = $this->connection->getMailbox($mail_box);
-
-	// 	$search = new SearchExpression();
-	// 	// if(is_array($query)){
-	// 	// 	foreach ($query as $q) {
-	// 	// 		$search->addCondition(new $query);
-	// 	// 	}
-	// 	// }else{
-	// 	// }
-	// 		$search->addCondition(new \Ddeboer\Imap\Search\Flag\Unseen);
-
-	// 	$mails = $mailbox->getMessages($search);
-		
-	// 	// $mails[0]->keepUnseen()->getBodyHtml();
-
-	// 	$return_mails=[];
-	// 	foreach ($mails as $mail) {
-	// 		$return_mails=[
-	// 			'body'=>[
-	// 					'text/html'=>$mail->keepUnseen()->getBodyHtml(),
-	// 					'text/plain'=>$mail->keepUnseen()->getBodyText()
-	// 					],
-
-	// 			'uid'=>$mail->getNumber(),	
-	// 			'uuid'=>$mail->getId(),	
-	// 			'subject'=>$mail->getSubject(),	
-	// 			'from_raw'=>$mail->getFrom(),	
-	// 			'to_raw'=>$mail->getTo(),	
-	// 			'cc_raw'=>$mail->getCc(),	
-	// 				];
-	// 		echo "<pre/>";
-	// 		var_dump($return_mails);
-	// 		exit;
-	// 	}
-
-	// 	return $return_mails;
-	// }
-
-	// function getUniqueEmails($uid, $getBody=true){
-	// 	return $this->imap->getUniqueEmails($uid);
-	// }
-
-	// function getMailBoxes(){
-	// 	return $this->connection->getMailboxes();
-	// }
 }
--- page/cron.php
+++ page/cron.php
@@ -16,46 +16,8 @@ class page_cron extends \Page {
 			$mbs = ['INBOX'] ; // $cont->getMailBoxes();
 
 			foreach ($mbs as $mb) {
-				$emails = $cont->fetch($mb);
-				// $emails = $cont->fetch($mb,'Ddeboer\Imap\Search\LogicalOperator\ALL',0,10,false,false);
-				// var_dump($emails);
-				// exit;
-				// foreach ($emails as $uid => $email) {
-				// 	if(!is_array($email)) continue;
-				// 	try{
-				// 		$body=(isset($email['body']['text/html']) And $email['body']['text/html'])?$email['body']['text/html']:(isset($email['body']['text/plain']) And $email['body']['text/plain'])?$email['body']['text/plain']:'(no content)';
-				// 		$email_model=$this->add('xepan\communication\Model_Communication_Email_Received');	
-				// 		$email_model['uid']=$uid;
-				// 		$email_model['uuid']=$email['id'];
-				// 		$email_model['mailbox']=$email_setting['imap_email_username'].'#'.$email['mailbox'];
-				// 		$email_model['from_raw']=json_encode($email['from']);
-				// 		$email_model['to_raw']=json_encode($email['to']);
-				// 		$email_model['cc_raw']=json_encode($email['cc']);
-				// 		$email_model['bcc_raw']=json_encode($email['bcc']);
-				// 		$email_model['flags']=$email['flags'];
-				// 		$email_model['title']=$email['subject'];
-				// 		$email_model['description']=$body;
-				// 		$email_model->findContact(false);
-
-				// 		if($email_model['from_id'] || $email_model['to_id']){
-				// 			$details = $cont->getUniqueEmails($uid);
-				// 			$email_model['description'] = $details['body']['text/html']?:$details['body']['text/plain'];
-				// 			$email_model['detailed']=true;
-
-				// 			if($details['attachment']){
-				// 				var_dump($details['body']['attachment']);
-				// 				var_dump($details['attachment']);
-				// 			}
-				// 		}
-				// 		$email_model->save();
-				// 		var_dump($email);
-				// 		exit;
-				// 	}catch(\Exception $e){
-				// 		var_dump($email);
-				// 		throw $e;
-				// 	}
-				// }
-
+				$emails_return = $cont->fetch($mb,'UNSEEN');
+				$this->app->hook('emails_fetched',[$emails_return]);
 			}
 		}
 
