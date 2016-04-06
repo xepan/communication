cdca922408ee400a596bb11f52f2d23b01e04bfd
vijay mali <guddavijay@gmail.com>
2016-04-06 12:50:32 +0530

    Add Complete Lister to add Multiple email or name value set to spot

--- lib/View/EmailDetail.php
+++ lib/View/EmailDetail.php
@@ -6,60 +6,18 @@ class View_EmailDetail extends \View{
 	}
 	function setModel($model){
 		$m=parent::setModel($model);
-		$this->template->setHTML('email_body',$model['description']);
-
 		$to_raw=$m['to_raw'];
-		$to_str = "";
+		$to_lister=$this->add('CompleteLister',null,'to_lister',['view/emails/email-detail','to_lister']);
+		// var_dump($m['to_raw']);
+		// exit;
+		$to_lister->setSource($to_raw);
 
-			foreach ($to_raw as $emails) {
-				if(isset($emails['name']) and $emails['name']){
-					$to_str.= $emails['name']."<".$emails['email'].">,";
-				}else{
-					$to_str.=$m['to'] ."<".$emails['email'].">,";
-				}	
-			}
-
-		$this->template->set('to_name',$to_str);
-		
 		$cc_raw=$m['cc_raw'];
-		$cc_str = "";
-			foreach ($cc_raw as $name) {
-				$email=$this->add('xepan\base\Model_Contact_Email');
-				$email->addCondition('value',$name['email']);
-				$email->tryLoadAny();
-				if($email->loaded()){
-					$contact=$email->ref('contact_id');
-				}
-				if(isset($name['name']) and $name['name']){
-					$cc_str.= $name['name']."<".$name['email'].">,";
-				}else{
-					// throw new \Exception($contact['name'], 1);
-					$cc_str.= $contact['name']."<".$name['email'].">,";
-					
-				}	
-			}
+		$cc_lister=$this->add('CompleteLister',null,'cc_lister',['view/emails/email-detail','cc_lister']);
+		$cc_lister->setSource($cc_raw);
 
-		$this->template->trySet('cc_name',$cc_str);
-
-		$bcc_raw=$m['bcc_raw'];
-		$bcc_str = "";
-			foreach ($bcc_raw as $name) {
-				$email=$this->add('xepan\base\Model_Contact_Email');
-				$email->addCondition('value',$name['email']);
-				$email->tryLoadAny();
-				if($email->loaded()){
-					$contact=$email->ref('contact_id');
-				}
-				if(isset($name['name']) and $name['name']){
-					$bcc_str.= $name['name']."<".$name['email'].">,";
-				}else{
-					$bcc_str.= $contact['name']."<".$name['email'].">,";
-
-				}
-			}
-
-		$this->template->trySet('bcc_name',$bcc_str);
-
+		$this->template->setHTML('email_body',$model['description']);
+	
 		return $m;
 	}
 	function defaultTemplate(){
