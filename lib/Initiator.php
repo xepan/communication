489eb06747e1155913c90d7635969db618a59800
vijay mali <guddavijay@gmail.com>
2016-04-09 12:44:17 +0530

    add contact email or All received  Email Count

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -11,8 +11,17 @@ class Initiator extends \Controller_Addon {
 		$this->addLocation(array('template'=>'templates','js'=>'templates/js'))
 			->setBaseURL('../vendor/xepan/communication/');
 
+		$contact_email=$this->add('xepan\communication\Model_Communication_Email_ContactReceivedEmail');
+
+		$contact_email->addCondition('extra_info',null);
+		$contact_count=$contact_email->count()->getOne();
+
+		$all_email=$this->add('xepan\communication\Model_Communication_Email_Received');
+		$all_email->addCondition('extra_info',null);
+		$all_count=$all_email->count()->getOne();
+		
 		if($this->app->is_admin){
-			$this->app->side_menu->addItem(['Emails','icon'=>' fa fa-users','badge'=>[123,'swatch'=>' label label-primary label-circle pull-right']],'xepan_communication_emails');
+			$this->app->side_menu->addItem(['Emails','icon'=>' fa fa-users','badge'=>[$contact_count. " / " .$all_count ,'swatch'=>' label label-primary label-circle pull-right']],'xepan_communication_emails');
 		}
 		
 	}
