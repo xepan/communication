c6b766b6fb00fc76134e3516d1cd372ad9d25b30
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-14 09:37:03 +0530

    Update last email sent at in email settings

--- lib/Model/Communication/Abstract/Email.php
+++ lib/Model/Communication/Abstract/Email.php
@@ -168,6 +168,9 @@ class Model_Communication_Abstract_Email extends Model_Communication{
 
 			$mailer->send($mail);
 
+			$email_setting['last_emailed_at'] = $this->app->now;
+			$email_setting->saveAndUnload();
+
 		}catch(\Exception $e){
 			$this->save();
 			throw $e;
