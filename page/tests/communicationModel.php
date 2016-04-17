c37f1ae6b4a03c53336ebd89d9c44c8e6ae22f8d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-17 23:11:30 +0530

    Created Test Cases and installer generator function

---
+++ page/tests/communicationModel.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+* description: ATK Page
+* 
+* @author : Gowrav Vishwakarma
+* @email : gowravvishwakarma@gmail.com, info@xavoc.com
+* @website : http://xepan.org
+* 
+*/
+
+namespace xepan\communication;
+
+class page_tests_communicationModel extends \xepan\base\Page_Tester {
+	public $title='Communication Model Testing';
+
+	function init(){
+		parent::init();
+		
+	}
+}
---
+++ page/tests/emailFetch.php
@@ -0,0 +1,34 @@
+<?php
+namespace xepan\communication;
+
+class page_tests_emailFetch extends \xepan\base\Page_Tester {
+    public $title = 'Communication Fetching Tests';
+
+    public $proper_responses=[
+
+        ];
+
+    function test_CheckConnectivity(){
+    	
+    }
+
+    function test_CheckProperConnectivityErrors(){
+
+    }
+
+    function test_EmailsFetched(){
+
+    }
+
+    function test_CommunicationModelsCreated(){
+
+    }
+
+    function test_CheckFindContact(){
+        // in email received
+        // Should be abstract email,
+        // should check To also 
+    }
+
+}
+?>
