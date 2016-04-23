f88c3c59c2895a03d17a7a6d231bce2902d75f97
vijay mali <guddavijay@gmail.com>
2016-04-23 12:11:41 +0530

    if mail status outbox save direction out

--- lib/Model/Communication/Abstract/Email.php
+++ lib/Model/Communication/Abstract/Email.php
@@ -137,6 +137,7 @@ class Model_Communication_Abstract_Email extends Model_Communication{
 
 	function send(\xepan\base\Model_Epan_EmailSetting $email_setting){
 		$this['status']='Outbox';
+		$this['direction']='Out';
 		$this['mailbox']=$email_setting['email_username'].'#SENT';
 		try{
 			
