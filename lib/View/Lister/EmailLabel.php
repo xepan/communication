daacfe5a0b3f8a511a5bb1630803efa09dc622fb
vijay mali <guddavijay@gmail.com>
2016-04-25 11:58:21 +0530

    email lables count display

--- lib/View/Lister/EmailLabel.php
+++ lib/View/Lister/EmailLabel.php
@@ -3,6 +3,18 @@ namespace xepan\communication;
 
 class View_Lister_EmailLabel extends \CompleteLister{
 
+	function formatRow(){
+		$email=$this->add('xepan\communication\Model_Communication_Email_Received');
+		$email->addCondition('direction','In');
+
+		$email->addCondition('mailbox',$this->model['email_username'].'#INBOX');
+		
+		$email_count=$email->count()->getOne();
+
+		$this->current_row['email_count']=$email_count ;
+		return parent::formatRow();
+	}
+
 	function getJSID(){
 		return 'email-nav-labels-wrapper';
 	}
