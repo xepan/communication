2522dd8c6d090baf77fc749c8a1be7c3616caa58
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-30 12:19:36 +0530

    find contact test case added

--- page/tests/communicationModel.php
+++ page/tests/communicationModel.php
@@ -14,8 +14,51 @@ namespace xepan\communication;
 class page_tests_communicationModel extends \xepan\base\Page_Tester {
 	public $title='Communication Model Testing';
 
-	function init(){
-		parent::init();
-		
+	public $proper_responses=[
+			'test_findContactTo1'=>['Buddy Trade','','','Prime Scan'],
+			'test_findContactFrom1'=>['','Prime Scan'],
+        ];
+
+	function test_findContactTo1(){
+		$result =[];
+		$email = $this->add('xepan\communication\Model_Communication_Abstract_Email');
+		$email['to_raw']='[{"name":"Buddy Trade","email":"jashwant.k.salvi@gmail.com"},{"name":"Buddy Trade","email":"mediafarm.udaipur@gmail.com"}]';
+		$email->findContact(false,'to');
+		$result [] = $email->ref('to_id')->get('name');
+
+		$email = $this->add('xepan\communication\Model_Communication_Abstract_Email');
+		$email['to_raw']='[]';
+		$email->findContact(false,'to');
+		$result [] = $email->ref('to_id')->get('name');
+
+		$email = $this->add('xepan\communication\Model_Communication_Abstract_Email');
+		$email['to_raw']='[{"name":null,"email":"management@xavoc.com"}]';
+		$email->findContact(false,'to');
+		$result [] = $email->ref('to_id')->get('name');
+
+		$email = $this->add('xepan\communication\Model_Communication_Abstract_Email');
+		$email['to_raw']='[{"name":"Prime Scan","email":"prime_scan@yahoo.com"},{"name":"Gowrav Vishwakarma","email":"gowravvishwakarma@gmail.com"},{"name":"Rakesh Sinha","email":"rksinha.btech@gmail.com"}]';
+		$email->findContact(false,'to');
+		$result [] = $email->ref('to_id')->get('name');
+
+		return $result;
+
+	}
+
+
+	function test_findContactFrom1(){
+		$result = [];
+
+		$email = $this->add('xepan\communication\Model_Communication_Abstract_Email');
+		$email['from_raw']='{"name":"Somya Martand","email":"somyamartand26@gmail.com"}';
+		$email->findContact(false,'from');
+		$result [] = $email->ref('to_id')->get('name');
+
+		$email = $this->add('xepan\communication\Model_Communication_Abstract_Email');
+		$email['from_raw']='{"name":"prime_scan","email":"prime_scan@yahoo.com"}';
+		$email->findContact(false,'from');
+		$result [] = $email->ref('from_id')->get('name');
+
+		return $result;
 	}
 }
