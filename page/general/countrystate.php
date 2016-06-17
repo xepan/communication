d641be6e4dfdb6b88d1ff48f7c26f9ffe61b2e11
dkanojia <dkanojia93@gmail.com>
2016-06-17 15:30:18 +0530

    validation checked

--- page/general/countrystate.php
+++ page/general/countrystate.php
@@ -6,29 +6,33 @@ class page_general_countrystate extends \xepan\communication\page_sidebar{
 	function init(){
 		parent::init();
 
-		$country_model = $this->add('xepan\base\Model_Country');
-		$country_model->setOrder('name','asc');
 
-		$crud = $this->add('xepan\hr\CRUD');
-		$crud->setModel($country_model);
-		$crud->grid->addQuickSearch(['name','iso_code']);
-		$crud->grid->addPaginator(50);
+			$country_model = $this->add('xepan\base\Model_Country');
+			$country_model->setOrder('name','asc');
 
-		$crud->grid->add('VirtualPage')
-			->addColumn('State')
-			->set(function($page){
-				$country_id = $_GET[$page->short_name.'_id'];
-				
-				$state_model = $page->add('xepan\base\Model_State')->addCondition('country_id',$country_id);
-				$state_model->setOrder('name','asc');
-
-				$crud = $page->add('xepan\hr\CRUD');
-				$crud->setModel($state_model);
-				$crud->grid->addQuickSearch(['name','abbreviation']);
+			if(!$this->api->auth->model->isSuperUser()){
+				$this->add('View_Error')->set('Sorry, you are not permitted to handle this section , Ask respective authority / SuperUser');
+				return;
+			}else{
+				$crud = $this->add('xepan\hr\CRUD');
+				$crud->setModel($country_model);
+				$crud->grid->addQuickSearch(['name','iso_code']);
 				$crud->grid->addPaginator(50);
 
- 		});
+				$crud->grid->add('VirtualPage')
+					->addColumn('State')
+					->set(function($page){
+						$country_id = $_GET[$page->short_name.'_id'];
+						
+						$state_model = $page->add('xepan\base\Model_State')->addCondition('country_id',$country_id);
+						$state_model->setOrder('name','asc');
 
-	
+						$crud = $page->add('xepan\hr\CRUD');
+						$crud->setModel($state_model);
+						$crud->grid->addQuickSearch(['name','abbreviation']);
+						$crud->grid->addPaginator(50);
+
+		 		});
+			}
 	}
 }
