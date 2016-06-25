6a37f8d1bf02fde215a3b8e07a9fd50828d58269
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-06-25 10:49:45 +0530

    call only if it is not ajax request

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -10,16 +10,20 @@ class Initiator extends \Controller_Addon {
 		$this->addLocation(array('template'=>'templates','js'=>'templates/js'))
 			->setBaseURL('../vendor/xepan/communication/');
 
-		$contact_email=$this->add('xepan\communication\Model_Communication_Email_ContactReceivedEmail');
+		if(!$this->app->isAjaxOutput()){
+			
+			$contact_email=$this->add('xepan\communication\Model_Communication_Email_ContactReceivedEmail');
 
-		$contact_email->addCondition('extra_info',null);
-		$contact_count=$contact_email->count()->getOne();
+			$contact_email->addCondition('extra_info',null);
+			$contact_count=$contact_email->count()->getOne();
 
-		$all_email=$this->add('xepan\communication\Model_Communication_Email_Received');
-		$all_email->addCondition('extra_info',null);
-		$all_count=$all_email->count()->getOne();
-		
-		$this->app->side_menu->addItem(['Emails','icon'=>' fa fa-envelope','badge'=>[$contact_count. " / " .$all_count ,'swatch'=>' label label-primary pull-right']],'xepan_communication_emails')->setAttr(['title'=>'Emails']);
+			$all_email=$this->add('xepan\communication\Model_Communication_Email_Received');
+			$all_email->addCondition('extra_info',null);
+			$all_count=$all_email->count()->getOne();
+			
+			$this->app->side_menu->addItem(['Emails','icon'=>' fa fa-envelope','badge'=>[$contact_count. " / " .$all_count ,'swatch'=>' label label-primary pull-right']],'xepan_communication_emails')->setAttr(['title'=>'Emails']);
+		}
+
 		$search_communication = $this->add('xepan\communication\Model_Communication');
 		$this->app->addHook('quick_searched',[$search_communication,'quickSearch']);
 		$this->app->addHook('contact_info',[$search_communication,'set_old_communication_info']);
