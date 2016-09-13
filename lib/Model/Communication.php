e36fd3bc01e4c41c70f07d8beee9dad72c6eb570
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-09-13 18:19:03 +0530

    related_2_id field not required

--- lib/Model/Communication.php
+++ lib/Model/Communication.php
@@ -40,7 +40,6 @@ class Model_Communication extends \xepan\base\Model_Table{
 		$this->addField('communication_type');
 
 		$this->addField('related_id'); // Can be used anywhere as per requirement
-		$this->addField('related_2_id'); // Can be used anywhere as per requirement
 		
 		$this->addField('sent_on')->type('date'); // Can be used anywhere as per requirement
 		
