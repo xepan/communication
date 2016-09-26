af33cce31550c0f87ded8c4b7f7d6bfa92237f8b
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-09-26 18:24:54 +0530

    ACL implemented in Company Info Json Model

--- page/generalsetting.php
+++ page/generalsetting.php
@@ -14,28 +14,28 @@ class page_generalsetting extends \xepan\communication\page_sidebar{
 			$setiingview=$this->add('xepan\hr\CRUD',['action_page'=>'xepan_communication_general_email'],'general_setting',['view/setting/email-setting-grid']);
 			$setiingview->setModel('xepan\communication\Communication_EmailSetting');
 			
-			/*SMS Setting*/
-			$sms_view=$this->add('xepan\hr\CRUD',null,'sms_setting',['view/setting/sms-setting-grid']);
-			if($sms_view->isEditing()){
-				$form=$sms_view->form;
-				$form->setLayout('view/setting/form/sms-setting');
-				$form->js(true)->find('button')->addClass('btn btn-primary');
-			}	
-			$sms_view->setModel('xepan\communication\Model_Communication_SMSSetting');
+			// /*SMS Setting*/
+			// $sms_view=$this->add('xepan\hr\CRUD',null,'sms_setting',['view/setting/sms-setting-grid']);
+			// if($sms_view->isEditing()){
+			// 	$form=$sms_view->form;
+			// 	$form->setLayout('view/setting/form/sms-setting');
+			// 	$form->js(true)->find('button')->addClass('btn btn-primary');
+			// }	
+			// $sms_view->setModel('xepan\communication\Model_Communication_SMSSetting');
 
-			/*MISC Setting*/
-			$misc_config = $this->app->epan->config;
-			$misc_time_zone = $misc_config->getConfig('TIME_ZONE');
-			$form = $this->add('Form_Stacked',null,'misc_view');
-			$time_zone_field=$form->addField('DropDown','time_zone')->set($misc_time_zone);
-			$time_zone_field->setValueList(array_combine(timezone_identifiers_list(),timezone_identifiers_list()));
-			$form->addSubmit('Update')->addClass('btn btn-primary');
+			// MISC Setting
+			// $misc_config = $this->app->epan->config;
+			// $misc_time_zone = $misc_config->getConfig('TIME_ZONE');
+			// $form = $this->add('Form_Stacked',null,'misc_view');
+			// $time_zone_field=$form->addField('DropDown','time_zone')->set($misc_time_zone);
+			// $time_zone_field->setValueList(array_combine(timezone_identifiers_list(),timezone_identifiers_list()));
+			// $form->addSubmit('Update')->addClass('btn btn-primary');
 
-			if($form->isSubmitted()){
-				$misc_config->setConfig('TIME_ZONE',$form['time_zone'],'base');
+			// if($form->isSubmitted()){
+			// 	$misc_config->setConfig('TIME_ZONE',$form['time_zone'],'base');
 				
-				$form->js(null,$form->js()->reload())->univ()->successMessage('Update Information')->execute();
-			}
+			// 	$form->js(null,$form->js()->reload())->univ()->successMessage('Update Information')->execute();
+			// }
 
 			/*Company Info*/
 
@@ -48,8 +48,7 @@ class page_generalsetting extends \xepan\communication\page_sidebar{
 									'company_email'=>"Line",
 									'company_address'=>"Line",
 									'company_pin_code'=>"Line",
-									'company_description'=>"text",
-									'company_description'=>"text",
+									'company_description'=>"xepan\base\RichText",
 									'company_logo_absolute_url'=>"Line",
 									'company_twitter_url'=>"Line",
 									'company_facebook_url'=>"Line",
@@ -59,14 +58,17 @@ class page_generalsetting extends \xepan\communication\page_sidebar{
 						'config_key'=>'COMPANY_AND_OWNER_INFORMATION',
 						'application'=>'communication'
 					]);
+			
+			$company_m->add('xepan\hr\Controller_ACL');
 			$company_m->tryLoadAny();
+
 			$c_form = $this->add('Form_Stacked',null,'company_info');
 			$c_form->setModel($company_m);
 			$c_form->addSubmit('Save')->addClass('btn btn-primary');
 			
 			if($c_form->isSubmitted()){
 				$c_form->save();
-				$c_form->js(null,$form->js()->reload())->univ()->successMessage('Update Information')->execute();
+				$c_form->js(null,$c_form->js()->reload())->univ()->successMessage('Update Information')->execute();
 			}
 
 			// $this->add('View',null,'company_info',['view/schema-micro-data','person_info'])->setModel($company_m);
