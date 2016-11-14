ed742704857b3e72a148f65ee8f26902aec4e6fb
dkanojia <dkanojia93@gmail.com>
2016-11-14 20:19:32 +0530

    communication widget added

--- lib/View/Widget/CommunicationLister.php
+++ lib/View/Widget/CommunicationLister.php
@@ -6,9 +6,18 @@ class View_Widget_CommunicationLister extends \xepan\base\Grid{
 	
 	function init(){
 		parent::init();
-		
+		$m = $this->model;
 	}
 
+	function formatRow(){
+		$m = $this->model;
+		$this->current_row_html['description'] = $m['description'];
+		return parent::formatRow();
+	}
+
+	function setModel($model){
+		parent::setModel($model);
+	}
 	function defaultTemplate(){
 		return['view\widget\communication\lister'];
 	}
