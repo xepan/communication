56b421f8408ab7da341378f40f8f5ff860e90d1e
dkanojia <dkanojia93@gmail.com>
2016-11-14 19:47:10 +0530

    marketing updated

--- lib/View/Widget/CommunicationLister.php
+++ lib/View/Widget/CommunicationLister.php
@@ -6,7 +6,7 @@ class View_Widget_CommunicationLister extends \xepan\base\Grid{
 	
 	function init(){
 		parent::init();
-		$communication = $this->add('xepan\communication\Model_Communication');
+		
 	}
 
 	function defaultTemplate(){
