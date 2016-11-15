ab44d483acf8434dd32d207a151d8ccc68025f47
vijay mali <guddavijay@gmail.com>
2016-11-15 17:44:12 +0530

    remove duplicate sidebar menu

--- page/sidebar.php
+++ page/sidebar.php
@@ -15,7 +15,7 @@ class page_sidebar extends \xepan\base\Page
         $x->addItem([' Admin User Setting','icon'=>' fa fa-users'],'xepan_communication_general_emailcontent_admin')->setAttr(['title'=>'Admin User Email Setting']);
         $x->addItem([' Frontend User Setting','icon'=>' fa fa-users'],'xepan_communication_general_emailcontent_usertool')->setAttr(['title'=>'Frontend User Email Setting']);
 		
-		$this->app->side_menu->addItem(['General Setting','icon'=>' fa fa-dashboard'],'xepan_communication_generalsetting')->setAttr(['title'=>'General Setting']);
+		// $this->app->side_menu->addItem(['General Setting','icon'=>' fa fa-dashboard'],'xepan_communication_generalsetting')->setAttr(['title'=>'General Setting']);
 		// $this->app->side_menu->addItem(['Admin Setting','icon'=>' fa fa-users'],'xepan_communication_general_emailcontent_admin')->setAttr(['title'=>'Admin User Email Setting']);
 		// $this->app->side_menu->addItem(['User Setting','icon'=>' fa fa-users'],'xepan_communication_general_emailcontent_usertool')->setAttr(['title'=>'Frontend User Email Setting']);
 		$this->app->side_menu->addItem(['Country/State','icon'=>' fa fa-users'],'xepan_communication_general_countrystate')->setAttr(['title'=>'Country/ State Management']);
