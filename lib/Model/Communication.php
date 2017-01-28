e3564defbab7917f4e37eb5c6af55a3dbda51fce
vijay mali <guddavijay@gmail.com>
2017-01-28 18:59:48 +0530

    inital update

--- lib/Model/Communication.php
+++ lib/Model/Communication.php
@@ -66,10 +66,12 @@ class Model_Communication extends \xepan\base\Model_Table{
 		$this->addField('extra_info')->defaultValue(['seen_by'=>[]]);
 		
 		$this->addField('type');
- 
+ 	
 		$this->hasMany('xepan\communication\Communication_Attachment','communication_id',null,'EmailAttachments');
 		$this->hasMany('xepan\crm\Ticket_Comments','communication_id',null,'Comments');
 		$this->hasMany('xepan\crm\SupportTicket','communication_id',null,'SupportTicket');
+		$this->hasMany('xepan\base\Contact_CommunicationReadEmail','communication_id',null,'UnreadEmails');
+
 		$this->addExpression('image')->set($this->refSQL('from_id')->fieldQuery('image'));
 		$this->addExpression('attachment_count')->set($this->refSQL('EmailAttachments')->addCondition('type','attach')->count());
 
