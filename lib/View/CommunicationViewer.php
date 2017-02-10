c13d25dddf25ab2725f68c7f04ff8eb4093fbacb
dkanojia <dkanojia93@gmail.com>
2017-02-10 11:32:22 +0530

    communication viewer updated

--- lib/View/CommunicationViewer.php
+++ lib/View/CommunicationViewer.php
@@ -6,8 +6,15 @@ class View_CommunicationViewer extends \View{
 
 		$id = $_GET['comm_id'];
 		$communication_model = $this->add('xepan\communication\Model_Communication');
+		$communication_model->tryLoadBy('id',$id);
+		
+		if(!$communication_model->loaded()){
+			$this->add('View')->setElement('H4')->set('Communication not found, It might be deleted')->addClass('project-box-header red-bg well-sm')->setstyle('color','white');
+			return;
+		}
+
 		$grid = $this->add('View',null,null,['view/communication/viewer']);
-		$grid->setModel($communication_model->load($id));
+		$grid->setModel($communication_model);
 		$grid->template->trySetHtml('detail',$communication_model['description']);
 	}
 }
