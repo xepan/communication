d1cb61b75d009fa329043525615dfd52211f7581
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-10-12 09:33:58 +0530

    working on new view communication

---
+++ lib/View/Communication.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace xepan\communication;
+
+
+
+class View_Communication extends \View {
+	
+	public $allowed_channels = ['email','call','call_sent','call_received','meeting','personal','comment'];
+
+	public $channel_email=true;
+	public $channel_call_sent=true;
+	public $channel_call_received=true;
+	public $channel_meeting=true;
+	public $channel_comment=true;
+
+	public $showCommunicationHistory = true;
+	public $showAddCommunications = true;
+
+	public $contact=null;
+
+	function init(){
+		parent::init();
+		$this->template->loadTemplateFromString($this->myTemplate());
+	}
+
+	function filter($from_ids=null,$to_ids=null,$related_document_ids=null,$created_by_ids=null){
+
+	}
+
+	function setCommunicationsWith($contact){
+		$this->contact = $contact;
+		$communication = $this->add('xepan\communication\Model_Communication');
+		$communication->addCondition([['from_id',$contact->id],['to_id',$contact->id]]);
+		$communication->setOrder('created_at','desc');
+
+		return $this->setModel($communication);
+	}
+
+	function setCommunicationsRelatedToDocument($document){
+
+	}
+
+
+	function setModel($model){
+		return parent::setModel($model);
+	}
+
+	function showCommunicationHistory($show){
+		$this->showCommunicationHistory= $show;
+	}
+
+	function showAddCommunications($show){
+		$this->showAddCommunications = $show;
+	}
+
+	function addChannels($channels){
+		if(is_array($channels)){
+			foreach ($channels as $ch) {
+				$this->addChannels($ch);
+			}
+			return;
+		}
+
+		if(!in_array(strtolower($channels), $this->channels)) 
+			throw $this->exception('Unknown channel')->addMoreInfo('Available Channels ',implode(", ", $this->channels))->addMoreInfo('Provided Channel',$channels);
+			
+		switch (strtolower($channels)) {
+			case 'email':
+				$this->channel_email = true;
+				break;
+			case 'call':
+				$this->channel_call_sent = true;
+				$this->channel_call_received = true;
+				break;
+			case 'call_sent':
+				$this->channel_call_sent = true;
+				break;
+			case 'call_received':
+				$this->channel_call_received = true;
+				break;
+			case 'meeting':
+			case 'personal':
+				$this->channel_meeting = true;
+				break;
+			case 'comment':
+				$this->channel_comment = true;
+				break;
+		}
+	}
+
+	function addTopBar(){
+		if($this->channel_email) {
+			$email_icon = $this->add('Icon',null,'icons')->set('fa fa-envelope fa-3x')->addStyle('cursor:hand');
+			$this->manageEmail($email_icon);
+		}
+		if($this->channel_call_sent){
+			$called_icon = $this->add('Icon',null,'icons')->set('fa fa-upload fa-3x');
+			$this->manageCalled($called_icon);
+		}
+		if($this->channel_call_received) $this->add('Icon',null,'icons')->set('fa fa-users');
+		if($this->channel_meeting) $this->add('Icon',null,'icons')->set('fa fa-users');
+		if($this->channel_comment) $this->add('Icon',null,'icons')->set('fa fa-users');
+	}
+
+	function addCommunicationHistory(){
+
+	}
+
+	function manageEmail($email_icon, $edit_communication= null){
+		$email_popup = $this->add('xepan\base\View_ModelPopup')->addClass('modal-full');
+		$email_popup->setTitle('Send New Email');
+		$default_to_ids=implode(",",$this->contact->getEmails());
+		$form = $email_popup->add('Form');
+		$form->add('xepan\base\Controller_FLC')
+			->makePanelsCoppalsible()
+			->closeOtherPanels()
+			->layout([
+				'subject'=>'Email Content|danger~c1~8',
+				'from_email_id'=>'c3~4',
+				'body'=>'c4~12',
+				'to'=>'Send To (' .$default_to_ids.  ')~c1~8~closed',
+				'cc'=>'c3~5',
+				'cc_me'=>'c35~1',
+				'bcc'=>'c4~5',
+				'bcc_me'=>'c5~1',
+				'task_title'=>'Followup/Score~c1~8~closed',
+				'score_buttons~Score'=>'c2~2',
+				'score~'=>'c3',
+				'followup_on'=>'c4~6',
+				'assigned_to'=>'c5~6',
+				'followup_detail'=>'c6~12',
+				'set_reminder'=>'c7~12',
+				'reminder_at'=>'c8~4',
+				'remind_via'=>'c9~4',
+				'notify_to'=>'c10~4',
+				'snooze_duration'=>'c11~2',
+				'snooze_unit~'=>'c12~2',
+
+			]);
+		$subject = $form->addField('subject')->validate('required');
+		$form->addField('xepan\base\RichText','body');
+		$form->addField('to')->set($default_to_ids);
+		
+		$cc = $form->addField('cc');
+		$cc_me = $form->addField('Checkbox','cc_me');
+		$cc_me->js('click',$cc->js()->val($this->app->employee->getEmails()));
+		
+		$bcc = $form->addField('bcc');
+		$bcc_me = $form->addField('Checkbox','bcc_me');
+		$bcc_me->js('click',$bcc->js()->val($this->app->employee->getEmails()));
+		
+		$form->addField('xepan\hr\EmployeeAllowedEmail','from_email_id');
+		$followup_on = $form->addField('DateTimePicker','followup_on');
+		$form->addField('xepan\hr\Employee','assigned_to');
+		$form->addField('Text','followup_detail');
+
+		$follow_title = $form->addField('task_title');
+		$score = $form->addField('Hidden','score');
+		$set = $form->layout->add('ButtonSet',null,'score_buttons');
+		$up_btn = $set->add('Button')->set('UP')->addClass('btn');
+		$down_btn = $set->add('Button')->set('DOWN')->addClass('btn');
+		
+
+		$form->addField('CheckBox','set_reminder');
+		$reminder_at = $form->addField('DateTimePicker','reminder_at');
+		$form->addField('DropDown','remind_via')->setValueList(['Email'=>'Email','SMS'=>'SMS','Notification'=>'Notification'])->setAttr(['multiple'=>'multiple'])->setEmptyText('Please Select A Value');
+		$form->addField('xepan\hr\Employee','notify_to')->setAttr(['multiple'=>'multiple']);
+		$form->addField('snooze_duration');
+		$form->addField('DropDown','snooze_unit')->setValueList(['Minutes'=>'Minutes','hours'=>'Hours','day'=>'Days'])->setEmptyText('Please select a value');
+
+		if($form->isSubmitted()){
+
+		}
+
+		// JAVASCRIP SECTION
+		$subject->js('change',"\$('#$follow_title->name').val('".$this->contact['name']." : ' + \$('#$subject->name').val())");
+		$up_btn->js('click',[$score->js()->val(1),$down_btn->js()->removeClass('btn-danger'),$this->js()->_selectorThis()->addClass('btn-success')]);
+		$down_btn->js('click',[$score->js()->val(-1),$up_btn->js()->removeClass('btn-success'),$this->js()->_selectorThis()->addClass('btn-danger')]);
+		$email_icon->js('click',[ // show event
+			$email_popup->js()->modal(['backdrop'=>true,'keyboard'=>true]),
+			$form->js(null,'$("#'.$form->name.'").find("form")[0].reset();'),
+			$followup_on->js()->val(''),
+			$reminder_at->js()->val(''),
+		]);
+	}
+
+	function manageCalled($called_icon){
+		$called_popup = $this->add('xepan\base\View_ModelPopup')->addClass('modal-full');
+		$called_popup->setTitle('Log Phone Called');
+
+		$called_icon->js('click',$called_popup->js()->modal(['backdrop'=>true,'keyboard'=>true]));
+	}
+
+	function recursiveRender(){
+		if($this->showAddCommunications) $this->addTopBar();
+		if($this->showCommunicationHistory) $this->addCommunicationHistory();
+		parent::recursiveRender();
+	} 
+
+	function myTemplate(){
+		$template='
+			<div id="{$_name}" class="{$class}">
+				<div class="top-bar">
+					{$icons}
+				</div>
+				{$Content}
+			</div>
+		';
+		return $template;
+	}
+}
--- page/test.php
+++ page/test.php
@@ -10,29 +10,8 @@ class page_test extends \xepan\base\Page{
 	function init(){
 		parent::init();
 
-		$message_model = $this->add('xepan\communication\Model_Communication_AbstractMessage');
-		$to_raw = [];
-		foreach ($message_model as $msg) {
-			foreach ($msg['to_raw'] as $to) {
-				$read_email = $this->add('xepan\base\Model_Contact_CommunicationReadEmail');
-				$read_email->addCondition('communication_id',$msg->id);
-				$read_email->addCondition('type',"TO");
-				$read_email->addCondition('is_read',true);
-				$read_email->addCondition('contact_id',$to['id']);
-				$read_email->tryLoadAny();
-				$read_email->save();
-			}
+		$comm = $this->add('xepan\communication\View_Communication');
+		$comm->setCommunicationsWith($this->app->employee);
 
-			foreach ($msg['cc_raw'] as $to) {
-				$read_email = $this->add('xepan\base\Model_Contact_CommunicationReadEmail');
-				$read_email->addCondition('communication_id',$msg->id);
-				$read_email->addCondition('type',"CC");
-				$read_email->addCondition('is_read',true);
-				$read_email->addCondition('contact_id',$to['id']);
-				$read_email->tryLoadAny();
-				$read_email->save();
-			}
-
-		}
 	}
 }
