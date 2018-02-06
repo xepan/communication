775fa32b9caa9695c6db8a515483eff201155427
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-02-07 00:40:48 +0530

    some performace issue

--- lib/View/Lister/EmailsList.php
+++ lib/View/Lister/EmailsList.php
@@ -14,12 +14,12 @@ class View_Lister_EmailsList extends \CompleteLister{
 		}else{
 			$this->current_row['starred']='';
 		}
-		$unread_email = $this->add('xepan\base\Model_Contact_CommunicationReadEmail');
-		$unread_email->addCondition('communication_id',$this->model->id);
-		$unread_email->addCondition('contact_id',$this->app->employee->id);
-		$unread_email->addCondition('is_read',true);
-		$unread_email->tryLoadAny();
-		if($unread_email->loaded()){
+		// $unread_email = $this->add('xepan\base\Model_Contact_CommunicationReadEmail');
+		// $unread_email->addCondition('communication_id',$this->model->id);
+		// $unread_email->addCondition('contact_id',$this->app->employee->id);
+		// $unread_email->addCondition('is_read',true);
+		// $unread_email->tryLoadAny();
+		if($this->model['is_read']){
 				$this->current_row['unread']='';
 		}else{
 				$this->current_row['unread']='unread';
@@ -44,15 +44,15 @@ class View_Lister_EmailsList extends \CompleteLister{
 		}
 
 		$mailbox=explode('#', $this->model['mailbox']);
-		$email_model=$this->add('xepan\communication\Model_Communication_EmailSetting');
-		$email_model->tryLoadBy('email_username',$mailbox);
+		// $email_model=$this->add('xepan\communication\Model_Communication_EmailSetting');
+		// $email_model->tryLoadBy('email_username',$mailbox[0]);
 
 
 		$this->current_row['body'] = strip_tags($this->current_row['body']);
 		if($this->model['status']=='Sent'){
-			$this->current_row['email_name'] =$email_model['name']." / ".$this->model['status'];
+			$this->current_row['email_name'] =$mailbox[0]." / ".$this->model['status'];
 		}else{
-			$this->current_row['email_name'] =$email_model['name'];
+			$this->current_row['email_name'] =$mailbox[0];
 		}
 
 		parent::formatRow();
