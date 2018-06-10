8f6060cc01fa399e51eb94369b461058b73ea5bd
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-06-10 13:26:24 +0530

    Basic configuration menu name is now System

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -31,7 +31,7 @@ class Initiator extends \Controller_Addon {
 	}
 
 	function populateConfigurationMenus(){
-		$m = $this->app->top_menu->addMenu('Basic Configurations');
+		$m = $this->app->top_menu->addMenu('System');
     	$m->addItem(['Company Information','icon'=>'fa fa-cog'],$this->app->url('xepan_communication_generalsetting_companyinfo'));
     	$m->addItem(['Email Settings','icon'=>'fa fa-envelope'],$this->app->url('xepan_communication_generalsetting_emailsetting'));
     	$m->addItem(['SMS Settings','icon'=>'fa fa-mobile'],$this->app->url('xepan_communication_generalsetting_smssettings'));
