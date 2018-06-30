1407f0dce677a716081dd2529badc35fb69960b8
sinharakesh <rksinha.btech@gmail.com>
2018-06-30 16:19:34 +0530

    sub type 2,3 label name corrected

--- lib/Model/Config/SubType.php
+++ lib/Model/Config/SubType.php
@@ -21,8 +21,8 @@ class Model_Config_SubType extends \xepan\base\Model_ConfigJsonModel{
 		$this->getElement('sub_type')->caption('Sub Type 1');
 		$this->getElement('calling_status')->caption('Sub Type 2');
 		$this->getElement('sub_type_1_label_name')->defaultValue('Product/ Service/ Related To');
-		$this->getElement('sub_type_1_label_name')->defaultValue('Communication Result');
-		$this->getElement('sub_type_1_label_name')->defaultValue('Communication Remark');
+		$this->getElement('sub_type_2_label_name')->defaultValue('Communication Result');
+		$this->getElement('sub_type_3_label_name')->defaultValue('Communication Remark');
 		// $this->getField('default_login_page')->defaultValue('login');
 		// $this->getField('system_contact_types')->defaultValue('Contact,Customer,Supplier,Employee');
 	}
