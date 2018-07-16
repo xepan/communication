09b669abd5b296eb1d97a08e5e936cd282d0e676
sinharakesh <rksinha.btech@gmail.com>
2018-07-16 10:42:03 +0530

    unique lead count expression bug resolved

--- lib/Model/EmployeeCommunication.php
+++ lib/Model/EmployeeCommunication.php
@@ -96,8 +96,7 @@ class Model_EmployeeCommunication extends \xepan\hr\Model_Employee{
 			$all = $this->add('xepan\communication\Model_Communication',['table_alias'=>'employeecommniallfrom']);
 			return $all->addCondition('created_at','>=',$this->from_date)
 						->addCondition('created_at','<',$this->api->nextDate($this->to_date))
-						// ->addCondition('from_id','<>',$q->getField('id'))
-						->addCondition('created_by_id',$q->getField('id'))
+						->addCondition([['to_id',$q->getField('id')],['created_by_id',$q->getField('id')]])
 						->_dsql()->del('fields')
 						->field('GROUP_CONCAT(DISTINCT(from_id))')
 						;
@@ -105,10 +104,10 @@ class Model_EmployeeCommunication extends \xepan\hr\Model_Employee{
 
 		$this->addExpression('unique_leads_to')->set(function($m,$q){
 			$all = $this->add('xepan\communication\Model_Communication',['table_alias'=>'employeecommniallto']);
-			return $all->addCondition('created_by_id',$q->getField('id'))
-						->addCondition('created_at','>=',$this->from_date)
+			return $all->addCondition('created_at','>=',$this->from_date)
 						->addCondition('created_at','<',$this->api->nextDate($this->to_date))
-						// ->addCondition('to_id','<>',$q->getField('id'))
+						->addCondition([['from_id',$q->getField('id')],['created_by_id',$q->getField('id')]])
+						// ->addCondition('created_by_id',$q->getField('id'))
 						->_dsql()->del('fields')
 						->field('GROUP_CONCAT(DISTINCT(to_id))')
 						;
